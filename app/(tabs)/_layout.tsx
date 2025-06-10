1108996e181f619b1e78bc30fcb08619750edbf3
‫Daniel Goldman <76954172+dangogit@users.noreply.github.com>
2025-06-10 20:46:06 +0800

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, CirclePlus as PlusCircle, Clock } from 'lucide-react-native';
+import { Chrome as Home, CirclePlus as PlusCircle, Clock } from 'lucide-react-native';
 import { useThemeColor } from '@/hooks/useThemeColor';
 
 export default function TabLayout() {
